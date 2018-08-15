b98c1e8040897524cac6804e1a67dce972b3ce83
threadkind <threadkind@gmail.com>
2018-08-15 15:08:52 -0500

    Move search bar after submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
 // https://source.unsplash.com/featured/?nature
+
+let searchTerm = document.querySelector('input');
+let form = document.querySelector('form');
+let main = document.querySelector('main');
+
+
+
+document.querySelector('form').addEventListener('submit', function(e){
+	e.preventDefault();
+	console.log(searchTerm.value);
+
+	form.classList.add('hide');
+
+	setTimeout(function(){
+		document.querySelector('form br').style.display = 'none';
+		form.classList.add('moveSearch');
+		form.classList.remove('hide');
+
+
+	}, 900);
+
+
+});
+
